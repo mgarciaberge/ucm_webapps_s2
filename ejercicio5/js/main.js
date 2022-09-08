ab922d54931ea8d15361e42caa55d0e720ec86fb
Manel Garcia Bergé <mgarciaberge@gmail.com>
2022-09-08 12:50:39 +0200

    bootstrap en ejercicio5

--- ejercicio5/js/main.js
+++ ejercicio5/js/main.js
@@ -5,6 +5,7 @@ function addItem (){
     
     // Añadir elementos a la lista
     const node = document.createElement("li");
+    node.setAttribute("class", "list-group-item");
     const textnode = document.createTextNode("Elemento " + numberOfElements ++);
     node.appendChild(textnode);
     document.getElementById("lista").appendChild(node);    
